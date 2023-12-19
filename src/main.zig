756e15b23924c792defdd845ba261d8b2b533b49
dbubel <50341559+dbubel@users.noreply.github.com>
2023-12-19 00:45:58 -0500

    zig is bad sometimes

--- src/main.zig
+++ src/main.zig
@@ -6,37 +6,38 @@ const std = @import("std");
 
 pub fn main() !void {
     var alloc = std.heap.GeneralPurposeAllocator(.{}){};
-    var gpa = alloc.allocator();
+    const gpa = alloc.allocator();
 
     const address = try std.net.Address.parseIp("127.0.0.1", 4000);
 
-    var s = Server.init(address, &gpa);
+    var s = Server.init(address, gpa);
     try s.run();
 }
 
 const Server = struct {
     const This = @This();
     address: std.net.Address = undefined,
-    allocator: *std.mem.Allocator = undefined,
+    allocator: std.mem.Allocator = undefined,
 
-    pub fn init(addr: std.net.Address, alloc: *std.mem.Allocator) This {
+    pub fn init(addr: std.net.Address, alloc: std.mem.Allocator) This {
         return .{ .address = addr, .allocator = alloc };
     }
 
     pub fn run(self: *This) !void {
-        var server = std.http.Server.init(self.allocator.*, .{ .reuse_address = true });
+        var server = std.http.Server.init(self.allocator, .{ .reuse_address = true });
         defer server.deinit();
 
         var thread_pool: std.Thread.Pool = undefined;
         defer thread_pool.deinit();
 
-        try thread_pool.init(.{ .allocator = self.allocator.*, .n_jobs = 12 });
+        try thread_pool.init(.{ .allocator = self.allocator, .n_jobs = 12 });
         try server.listen(self.address);
+        // var resp_pool = std.heap.MemoryPool(std.http.Server.Response).init(self.allocator);
 
         std.debug.print("waiting on connections...\n", .{});
         while (true) {
-            var resp: std.http.Server.Response = try std.http.Server.accept(&server, .{ .allocator = self.allocator.*, .header_strategy = .{ .dynamic = 1024 } });
-            thread_pool.spawn(handleConnection, .{&resp}) catch |err| {
+            const resp: std.http.Server.Response = try std.http.Server.accept(&server, .{ .allocator = self.allocator });
+            thread_pool.spawn(handleConnection, .{resp}) catch |err| {
                 std.log.err("error spawning thread {any}", .{err});
             };
         }
@@ -45,26 +46,27 @@ const Server = struct {
 
 const handler = fn () void;
 
-fn handleConnection(resp: *std.http.Server.Response) void {
-    defer resp.deinit();
-    _ = resp.wait() catch |err| {
+fn handleConnection(resp: std.http.Server.Response) void {
+    var respCopy = resp; // this is very dumb i have to do this
+    defer respCopy.deinit();
+    _ = respCopy.wait() catch |err| {
         std.log.err("error wait {any}", .{err});
         return;
     };
 
-    _ = resp.send() catch |err| {
+    _ = respCopy.send() catch |err| {
         std.log.err("error send {any}", .{err});
         return;
     };
 
     const a = "hello from server";
-    resp.transfer_encoding = .{ .content_length = a.len };
-    _ = resp.writeAll(a) catch |err| {
+    respCopy.transfer_encoding = .{ .content_length = a.len };
+    _ = respCopy.writeAll(a) catch |err| {
         std.log.err("error writeAll {any}", .{err});
         return;
     };
 
-    _ = resp.finish() catch |err| {
+    _ = respCopy.finish() catch |err| {
         std.log.err("error finish {any}", .{err});
         return;
     };
