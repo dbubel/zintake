6ca104280578faae884b7e028219e2fa97530740
dbubel <50341559+dbubel@users.noreply.github.com>
2024-01-06 20:02:03 -0500

    middle hell

--- src/temp.zig
+++ src/temp.zig
@@ -1,24 +1,28 @@
 const std = @import("std");
+const handler = *const fn (u32) void;
 
-const middlewareFunc = fn (*const fn (u32) void, u32) void;
+const middlewareFunc = fn (handler) handler;
 
-fn firstMiddleware(next: *const fn (u32) void, data: u32) void {
-    std.debug.print("First middleware with data: {}\n", .{data});
-    next(data);
+fn firstMiddleware(h: handler) handler {
+    _ = h;
+    return fn (u32) void{};
 }
-
-fn secondMiddleware(next: *const fn (u32) void, data: u32) void {
-    std.debug.print("Second middleware with data: {}\n", .{data});
-    next(data);
-}
-
-fn finalFunction(data: u32) void {
-    std.debug.print("Final function with data: {}\n", .{data});
-}
+//
+// fn secondMiddleware(next: *const fn (u32) void) void {
+//     std.debug.print("Second middleware with data: \n", .{});
+//     next(1);
+// }
+//
+// fn finalFunction(data: u32) void {
+//     std.debug.print("Final function with data: {}\n", .{data});
+// }
 
 pub fn main() void {
-    const chain = [_]*const middlewareFunc{ &firstMiddleware, &secondMiddleware };
-    for (chain) |func| {
-        std.debug.print("func {any}\n", .{func});
-    }
+    const j: middlewareFunc = firstMiddleware;
+    _ = j;
+    // firstMiddleware(x(2));
+    // const chain = [_]*const middlewareFunc{ &firstMiddleware, &secondMiddleware };
+    // for (chain) |func| {
+    //     std.debug.print("func {any}\n", .{func});
+    // }
 }
